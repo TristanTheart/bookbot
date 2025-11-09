481c1f240c977537565171f066324d22e2b76521
TristanTheart <tristantheart@gmail.com>
2025-11-09 22:43:19 +0200

    refactored and added functionality

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    book_text = get_book_text(sys.argv[1])
+    counts = count_chars(book_text)
+    chars_list = to_list(counts)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at ")
+    print("----------- Word Count ----------")
     print(f"Found {get_num_words(book_text)} total words")
-    
+    print("--------- Character Count -------")
+    for item in chars_list:
+        ch = item["char"]
+        num = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {num}")
+    print("============= END ===============")
+
 
 def get_book_text(filepath):
 
@@ -11,5 +31,7 @@ def get_book_text(filepath):
     return file_contents
 
 from stats import get_num_words # pyright: ignore[reportMissingImports]
+from stats import count_chars # pyright: ignore[reportMissingImports]
+from stats import to_list # pyright: ignore[reportMissingImports]
 
 main()
