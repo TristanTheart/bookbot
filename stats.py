a83f02c736b97d7c04056978728caeb35fd54dfa
TristanTheart <tristantheart@gmail.com>
2025-11-09 22:40:31 +0200

    added to_list to return a sorted list of dictionaries

--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(book_text):
+    
+    char_dict = {}
+    for char in book_text:
+        if char.isalpha():
+            char = char.lower()
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    return char_dict
+
+def to_list(char_dict):
+    char_list = []
+    for ch, n in char_dict.items():
+        char_list.append({"char": ch, "num": n})
+    return sorted(char_list, key=lambda x: x["num"], reverse=True)
+
